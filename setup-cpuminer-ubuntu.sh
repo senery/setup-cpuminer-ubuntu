34fe445d78c2950237d8b01c66902fff81d2b245
senery <jd.renes@gmail.com>
2015-07-26 02:21:13 +0200

    Update setup-cpuminer-ubuntu.sh

--- setup-cpuminer-ubuntu.sh
+++ setup-cpuminer-ubuntu.sh
@@ -1,11 +1,9 @@
-
 sudo apt-get update
-sudo mkdir /home/mine
-cd /home/mine
 sudo apt-get install libcurl4-openssl-dev libncurses5-dev pkg-config automake yasm git make -y --force-yes
 sudo git clone https://github.com/pooler/cpuminer.git
 cd cpuminer
 sudo ./autogen.sh
 sudo ./configure CFLAGS="-O3"
 sudo make
-nohup /home/mine/cpu*/minerd --url=stratum+tcp://ltc.ghash.io:3333 --user=senery.virt1x --pass=123 &
+cd ~
+nohup ./cpuminer/minerd --url=stratum+tcp://scrypt.ltcrabbit.com:3333 --user=senery.aws2 --pass=x -q &
