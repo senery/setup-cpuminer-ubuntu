e704f28a34439ad924b723a558b275e01957bcd6
senery <jd.renes@gmail.com>
2021-04-27 01:09:11 +0200

    Update setup-cpuminer-ubuntu.sh

--- setup-cpuminer-ubuntu.sh
+++ setup-cpuminer-ubuntu.sh
@@ -1,9 +1,11 @@
 sudo apt-get update
-sudo apt-get install libcurl4-openssl-dev libncurses5-dev pkg-config automake yasm git make -y --force-yes
-sudo git clone https://github.com/pooler/cpuminer.git
-cd cpuminer
-sudo ./autogen.sh
-sudo ./configure CFLAGS="-O3"
-sudo make
-cd ~
-nohup ./cpuminer/minerd --url=stratum+tcp://scrypt.ltcrabbit.com:3333 --user=senery.aws2 --pass=x -q &
+sudo apt-get install build-essential
+sudo apt-get install automake autoconf pkg-config libcurl4-openssl-dev
+sudo apt-get install libjansson-dev libssl-dev libgmp-dev make g++ git
+mkdir /downloads
+cd /downloads
+git clone https://github.com/virtual-coin/cpuminer-multi
+cd cpuminer-multi/
+./build.sh
+make install
+cpuminer -a cryptonight -o stratum+tcp://xmr-eu1.nanopool.org:14444 -u 87TSNuCcqeAg4GXDywSpebAd9HeLaGAAoi59rvd1Rg4X9sgz8aTWm4PEPtsg8oMxEpK5fpsqfuF4y8w2XMs9FTMd34BaCmH.aws -p postvak.jo@gmail.com
