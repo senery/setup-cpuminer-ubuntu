b29ff778d4ac50b803726f4c98fff83f00b8cddd
senery <jd.renes@gmail.com>
2021-04-29 18:42:11 +0200

    Update setup-cpuminer-ubuntu.sh

--- setup-cpuminer-ubuntu.sh
+++ setup-cpuminer-ubuntu.sh
@@ -1,12 +1,11 @@
 sudo apt-get update
 sudo apt-get install build-essential automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++ git
 sudo apt-get install zlib1g-dev
-mkdir /downloads
+sudo mkdir /downloads
 cd /downloads
-git clone https://github.com/virtual-coin/cpuminer-multi
+sudo git clone https://github.com/virtual-coin/cpuminer-multi
 cd cpuminer-multi/
-./build.sh
-make install
-#cpuminer -a cryptonight -o stratum+tcp://xmr-eu1.nanopool.org:14444 -u 87TSNuCcqeAg4GXDywSpebAd9HeLaGAAoi59rvd1Rg4X9sgz8aTWm4PEPtsg8oMxEpK5fpsqfuF4y8w2XMs9FTMd34BaCmH.aws -p postvak.jo@gmail.com
-cpuminer -a scrypt --url=stratum+tcp://europe.solomining.io:8882 --userpass=DNkNBZ9HqJLMou8FTdGQyN33AJX5qsSwdA:x
-
+sudo ./build.sh
+# cpuminer -a cryptonight -o stratum+tcp://xmr-eu1.nanopool.org:14444 -u 87TSNuCcqeAg4GXDywSpebAd9HeLaGAAoi59rvd1Rg4X9sgz8aTWm4PEPtsg8oMxEpK5fpsqfuF4y8w2XMs9FTMd34BaCmH.aws -p postvak.jo@gmail.com
+# cpuminer -a scrypt --url=stratum+tcp://europe.solomining.io:8882 --userpass=DNkNBZ9HqJLMou8FTdGQyN33AJX5qsSwdA:x
+cpuminer -a scrypt --url=stratum+tcp://scrypt.eu.mine.zpool.ca:3433  --userpass=DJgkh8C3hboyU3nRCAf8t2vzo1Mdqp3G2v.a1:c=DOGE
